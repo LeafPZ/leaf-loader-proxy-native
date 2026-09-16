382cdab3e47be43e850fb5eeed6b7f3756327a21
aoqia <aoqia@aoqia.dev>
2026-09-16 19:57:19 +1000

    feat: implement it roughly

--- build.zig
+++ build.zig
@@ -6,17 +6,6 @@ pub fn build(b: *std.Build) void {
         .preferred_optimize_mode = .ReleaseSmall,
     });
 
-    const lib = b.addLibrary(.{
-        .name = "leaf_loader_proxy_native",
-        .linkage = .dynamic,
-        .root_module = b.createModule(.{
-            .root_source_file = b.path("src/main.zig"),
-            .target = target,
-            .optimize = optimize,
-            .imports = &.{},
-        }),
-    });
-
     const java_trans = b.addTranslateC(.{
         .root_source_file = b.path("lib/java.h"),
         .target = target,
@@ -29,7 +18,28 @@ pub fn build(b: *std.Build) void {
         std.log.warn("No JAVA_HOME environment variable specified. Zig may not be able to build against Java's include headers", .{});
     }
 
-    lib.root_module.addImport("java", java_trans.createModule());
+    const lib_mod = b.addModule("leaf_lodaer_proxy_native", .{
+        .root_source_file = b.path("src/main.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
+
+    lib_mod.addImport("java", java_trans.createModule());
+
+    const lib = b.addLibrary(.{
+        .name = "leaf_loader_proxy_native",
+        .linkage = .dynamic,
+        .root_module = lib_mod,
+    });
 
     b.installArtifact(lib);
+
+    const lib_check = b.addLibrary(.{
+        .name = "leaf_loader_proxy_native",
+        .linkage = .dynamic,
+        .root_module = lib_mod,
+    });
+
+    const check = b.step("check", "Check if the library compiles");
+    check.dependOn(&lib_check.step);
 }
--- src/main.zig
+++ src/main.zig
@@ -3,13 +3,30 @@ const win = std.os.windows;
 
 const java = @import("java");
 
-extern "kernel32" fn AddDllDirectory(NewDirectory: win.PCWSTR) callconv(.winapi) *opaque {};
+const W = std.unicode.utf8ToUtf16LeStringLiteral;
 
-pub export fn Agent_OnLoad(_: [*c][*c]const java.struct_JNIInvokeInterface_, _: [*c]u8, _: ?*anyopaque) java.jint {
-    std.debug.print("Agent loaded\n", .{});
+extern "kernel32" fn SetDllDirectoryW(lpPathName: ?win.LPCSTR) callconv(.winapi) win.BOOL;
+
+extern "user32" fn MessageBoxW(hWnd: ?win.HWND, lpText: ?win.LPCWSTR, lpCaption: ?win.LPCWSTR, uType: win.UINT) callconv(.winapi) c_int;
+
+pub export fn Agent_OnLoad(_: [*c][*c]const java.struct_JNIInvokeInterface_, options: [*c]u8, _: ?*anyopaque) java.jint {
+    var ret: win.BOOL = win.BOOL.FALSE;
+    if (options != null and std.mem.len(options) != 0) {
+        ret = SetDllDirectoryW(options);
+    } else {
+        ret = SetDllDirectoryW(".\\jre64\\bin");
+    }
+
+    if (ret == win.BOOL.FALSE) {
+        std.log.info("Failed to add DLL directory '{s}'", .{options});
+        _ = MessageBoxW(null, W("Agent failed to add the dll directory"), W("Leaf Native"), 0);
+        return java.JNI_ERR;
+    }
+
+    std.log.info("Agent loaded\n", .{});
     return java.JNI_OK;
 }
 
 pub export fn Agent_OnUnload() void {
-    std.debug.print("Agent unloaded\n", .{});
+    std.log.info("Agent unloaded\n", .{});
 }
